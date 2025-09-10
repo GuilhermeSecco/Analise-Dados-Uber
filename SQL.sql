c685cd024fa11bed8924c1e32f957645887f0f6c
Guilherme Fernandes Secco <155193683+GuilhermeSecco@users.noreply.github.com>
2025-09-10 16:00:30 -0300

    Tradução de alguns campos

--- SQL.sql
+++ SQL.sql
@@ -4,6 +4,58 @@ use uber;
 
 select * from corridas;
 
+select distinct motivoCancelamentoCliente from corridas;
+
+update corridas
+set motivoCancelamentoCliente = 'Motorista não estava se dirigindo ao local de inicio'
+where motivoCancelamentoCliente = 'Driver is not moving towards pickup location';
+
+update corridas
+set motivoCancelamentoCliente = 'Motorista pediu para cancelar'
+where motivoCancelamentoCliente = 'Driver asked to cancel';
+
+update corridas
+set motivoCancelamentoCliente = 'Ar Condicionado Não Funcionando'
+where motivoCancelamentoCliente = 'AC is not working';
+
+update corridas
+set motivoCancelamentoCliente = 'Mudança de Planos'
+where motivoCancelamentoCliente = 'Change of plans';
+
+update corridas
+set motivoCancelamentoCliente = 'Endereço Errado'
+where motivoCancelamentoCliente = 'Wrong Address';
+
+select distinct motivoCancelamentoMotorista from corridas;
+
+update corridas
+set motivoCancelamentoMotorista = 'Problema pessoal ou relacionado ao carro'
+where motivoCancelamentoMotorista = 'Personal & Car related issues';
+
+update corridas
+set motivoCancelamentoMotorista = 'Problema com o cliente'
+where motivoCancelamentoMotorista = 'Customer related issue';
+
+update corridas
+set motivoCancelamentoMotorista = 'Número de pessoas acima do permitido'
+where motivoCancelamentoMotorista = 'More than permitted people in there';
+
+update corridas
+set motivoCancelamentoMotorista = 'Cliente tossindo ou doente'
+where motivoCancelamentoMotorista = 'The customer was coughing/sick';
+
+update corridas
+set motivoNaoFinalizado = 'Veículo Quebrado'
+where motivoNaoFinalizado = 'Vehicle Breakdown';
+
+update corridas
+set motivoNaoFinalizado = 'Pedido pelo Cliente'
+where motivoNaoFinalizado = 'Customer Demand';
+
+update corridas
+set motivoNaoFinalizado = 'Outro Motivo'
+where motivoNaoFinalizado = 'Other Issue';
+
 select dia as "Data", count(idCorrida) as "Total de Corridas" from corridas
 group by Dia
 order by count(idCorrida) desc
@@ -16,15 +68,17 @@ limit 5;
 
 select avg(valorViagem) as "Preço médio das viagens" from corridas;
 
-select avg(distanciaViagem) "Distância média das viagens" from corridas;
+select avg(distanciaViagem) as "Distância média das viagens" from corridas;
 
 select localInicio as "Local de Inicio", count(idCorrida) as "Total de Corridas" from corridas
 group by localInicio
-order by count(idCorrida) desc;
+order by count(idCorrida) desc
+limit 5;
 
 select localFim as "Local de Chegada", count(idCorrida) as "Total de Corridas" from corridas
 group by localFim
-order by count(idCorrida) desc;
+order by count(idCorrida) desc
+limit 5;
 
 select veiculo as "Tipo de Veiculo", count(idCorrida) as "Total de Corridas", avg(notaMotorista) as "Nota média do veículo" from corridas
 group by veiculo
